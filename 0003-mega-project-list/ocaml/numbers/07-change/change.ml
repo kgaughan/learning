98b9b187c1d54d6616685e1b41512a803a83337e
Keith Gaughan <k@stereochro.me>
2014-05-08 04:14:56 +0100

    Change calculator.

---
+++ 0003-mega-project-list/ocaml/numbers/07-change/change.ml
@@ -0,0 +1,23 @@
+(* Change calculation. *)
+
+(* Typical European change denominations. *)
+let coins = [200; 100; 50; 20; 10; 5; 2; 1];;
+
+(* Returns a list of tuples in the form [(unit, amount)] *)
+let rec change amt coins =
+  match coins with
+  | [] -> []
+  | h :: t ->
+      if h <= amt then
+        (h, amt / h) :: (change (amt mod h) t)
+      else
+        (change amt t);;
+
+let main () =
+  print_string "Change (in cents)? ";
+  let amt = read_int () in
+  let amts = change amt coins in 
+  print_string "Your change is:\n";
+  List.iter (fun (u, a) -> Printf.printf "%d of %d\n" a u) amts;;
+
+main ();;
