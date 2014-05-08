536e6d21d427b889afaeb22d3ff7c3c1a0f3cf50
Keith Gaughan <k@stereochro.me>
2014-05-08 01:58:44 +0100

    Integer factorisation.

--- 0003-mega-project-list/ocaml/numbers/03-factorisation/fact.ml
+++ 0003-mega-project-list/ocaml/numbers/03-factorisation/fact.ml
@@ -1,10 +1,38 @@
-(*
- * Reasonably efficient integer factorisation.
- *
- * The algorithm is a variation on trial division. First we try all the primes
- * less than sqrt(n) + 1 (the biggest potential number that might be a prime
- * factor without the number itself being prime), dividing our primes into our
- * original number and recording any which divide cleanly. If, by the end,
- * we've generated no prime factors, then our original number was a prime, so
- * we append that to our list.
- *)
+(* Integer factorisation. *)
+
+open List;;
+
+let rec print_int_list =
+  List.iter (fun n -> print_int n; print_string " ");;
+
+let is_composite primes m =
+  List.exists (fun prime -> m mod prime == 0) primes;;
+
+(* Simple prime sieve generating all the primes up to n. *)
+let sieve n =
+  let rec sieve_rec primes m =
+    if m > n then
+      primes
+    else
+      sieve_rec (if is_composite primes m then primes else (m :: primes))
+                (m + 1)
+  in sieve_rec [] 2;;
+
+(* Perform integer factorisation on `n`. *)
+let int_factors n =
+  let rec int_fact remaining m =
+    match remaining with
+    | h :: t -> if m mod h == 0
+                then h :: (int_fact remaining (m / h))
+                else (int_fact t m)
+    | _ -> []
+  in
+  int_fact (sieve n) n;;
+
+let main () =
+  let arg = int_of_string Sys.argv.(1) in
+  print_int_list (int_factors arg);
+  print_newline ();
+  exit 0;;
+
+main ();;
