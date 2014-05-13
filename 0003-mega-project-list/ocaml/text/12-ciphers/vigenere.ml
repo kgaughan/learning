2097dda59812d91fb4f616fbd19250d714dfb830
Keith Gaughan <k@stereochro.me>
2014-05-13 03:29:30 +0100

    Add decryption.

--- 0003-mega-project-list/ocaml/text/12-ciphers/vigenere.ml
+++ 0003-mega-project-list/ocaml/text/12-ciphers/vigenere.ml
@@ -1,12 +1,16 @@
 (* Vigenère cipher *)
 
-let recta x y =
+let encrypt offset_x offset_y = offset_x + offset_y
+
+let decrypt offset_x offset_y = offset_x + 26 - offset_y
+
+let recta mode x y =
   let a = Char.code 'A' in
   let offset_x = Char.code x - a in
   let offset_y = Char.code y - a in
-  Char.chr ((offset_x + offset_y) mod 26 + a)
+  Char.chr ((mode offset_x offset_y) mod 26 + a)
 
-let encrypt key text =
+let cipher recta key text =
   let enc_ch i ch = recta ch key.[i mod String.length key] in
   let buf = Buffer.create (String.length text) in
   for i = 0 to String.length text - 1 do
@@ -15,6 +19,11 @@ let encrypt key text =
   Buffer.contents buf
 
 let _ =
-  let key = Sys.argv.(1) in
-  let text = Sys.argv.(2) in
-  print_endline (encrypt key text)
+  let recta =
+    match Sys.argv.(1) with
+    | "enc" -> recta encrypt
+    | "dec" -> recta decrypt
+    | _ -> exit 1 in
+  let key = Sys.argv.(2) in
+  let text = Sys.argv.(3) in
+  print_endline (cipher recta key text)
