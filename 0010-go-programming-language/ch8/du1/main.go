d15ed52a1593afa2ea82a570d75054aedb104a2e
Keith Gaughan <k@stereochro.me>
2019-08-18 02:56:34 +0100

    Last of chapter 8.

--- 0010-go-programming-language/ch8/du1/main.go
+++ 0010-go-programming-language/ch8/du1/main.go
@@ -6,7 +6,11 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
+
+var verbose = flag.Bool("v", false, "show verbose progress messages")
 
 func main() {
 	// Determine the initial directories.
@@ -16,22 +20,51 @@ func main() {
 		roots = []string{"."}
 	}
 
-	// Traverse the file tree.
-	fileSizes := make(chan int64)
+	// Cancel traversal when input is detected.
 	go func() {
-		for _, root := range roots {
-			walkDir(root, fileSizes)
-		}
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		close(done)
+	}()
+
+	// Traverse each root of the tree in parallel.
+	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
+	go func() {
+		n.Wait()
 		close(fileSizes)
 	}()
 
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+
 	// Print the results.
 	var nfiles, nbytes int64
-	for size := range fileSizes {
-		nfiles++
-		nbytes += size
+loop:
+	for {
+		select {
+		case <-done:
+			// Drain fileSizes to allow existing goroutines to finish.
+			for range fileSizes {
+				// do nothing
+			}
+			return
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop // fileSizes was closed
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
 	}
-	printDiskUsage(nfiles, nbytes)
+	printDiskUsage(nfiles, nbytes) // final totals
 }
 
 func printDiskUsage(nfiles, nbytes int64) {
@@ -40,23 +73,51 @@ func printDiskUsage(nfiles, nbytes int64) {
 
 // walkDir recursivesly walks the file tree rooted at dir and sends the size of
 // each found file on fileSizes.
-func walkDir(dir string, fileSizes chan<- int64) {
+func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
+	defer n.Done()
+	if cancelled() {
+		return
+	}
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
+			n.Add(1)
 			subdir := filepath.Join(dir, entry.Name())
-			walkDir(subdir, fileSizes)
+			go walkDir(subdir, n, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
 	}
 }
 
+// sema is a counting semaphore for limiting concurrency in dirents.
+var sema = make(chan struct{}, 20)
+
 // dirents returns the entries of directory dir.
 func dirents(dir string) []os.FileInfo {
+	select {
+	case sema <- struct{}{}: // acquire token
+	case <-done:
+		return nil // cancelled
+	}
+	defer func() {
+		<-sema // release token
+	}()
+
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
 		return nil
 	}
 	return entries
+}
+
+var done = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
 }
