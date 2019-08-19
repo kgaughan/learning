a3535962b356b91beb24017c8ed9fffc028cbddb
Keith Gaughan <k@stereochro.me>
2019-08-19 03:43:50 +0100

    Ch 9: safe memoisation.

--- 0010-go-programming-language/ch9/memo/memo.go
+++ 0010-go-programming-language/ch9/memo/memo.go
@@ -2,9 +2,12 @@
 // Func.
 package memo
 
+import "sync"
+
 // A Memo caches the results of calling a Func.
 type Memo struct {
 	f     Func
+	mu    sync.Mutex
 	cache map[string]result
 }
 
@@ -20,12 +23,19 @@ func New(f Func) *Memo {
 	return &Memo{f: f, cache: make(map[string]result)}
 }
 
-// NOTE: not concurrency-safe!
 func (memo *Memo) Get(key string) (interface{}, error) {
+	memo.mu.Lock()
 	res, ok := memo.cache[key]
+	memo.mu.Unlock()
+
 	if !ok {
 		res.value, res.err = memo.f(key)
+
+		// Between the two critical sections, several goroutines may race to
+		// compute f(key) and update the map.
+		memo.mu.Lock()
 		memo.cache[key] = res
+		memo.mu.Unlock()
 	}
 	return res.value, res.err
 }
