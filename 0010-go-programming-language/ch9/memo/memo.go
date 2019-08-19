967789eab81e700aa8613b9857f9c92dea17377b
Keith Gaughan <k@stereochro.me>
2019-08-19 03:56:58 +0100

    Ch 9: safe memoisation with duplicate suppression.

--- 0010-go-programming-language/ch9/memo/memo.go
+++ 0010-go-programming-language/ch9/memo/memo.go
@@ -4,11 +4,16 @@ package memo
 
 import "sync"
 
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
+}
+
 // A Memo caches the results of calling a Func.
 type Memo struct {
 	f     Func
-	mu    sync.Mutex
-	cache map[string]result
+	mu    sync.Mutex // guards cache
+	cache map[string]*entry
 }
 
 // Func is the type of the function to memoise.
@@ -20,22 +25,28 @@ type result struct {
 }
 
 func New(f Func) *Memo {
-	return &Memo{f: f, cache: make(map[string]result)}
+	return &Memo{f: f, cache: make(map[string]*entry)}
 }
 
 func (memo *Memo) Get(key string) (interface{}, error) {
 	memo.mu.Lock()
-	res, ok := memo.cache[key]
-	memo.mu.Unlock()
-
-	if !ok {
-		res.value, res.err = memo.f(key)
-
-		// Between the two critical sections, several goroutines may race to
-		// compute f(key) and update the map.
-		memo.mu.Lock()
-		memo.cache[key] = res
+	e := memo.cache[key]
+	if e == nil {
+		// This is the first request for this key. This goroutine becomes
+		// responsible for computing the value and broadcasting the ready
+		// condition.
+		e = &entry{ready: make(chan struct{})}
+		memo.cache[key] = e
 		memo.mu.Unlock()
+
+		e.res.value, e.res.err = memo.f(key)
+
+		close(e.ready) // broadcast ready condition
+	} else {
+		// This is a repeat request for this key.
+		memo.mu.Unlock()
+
+		<-e.ready // wait for ready condition
 	}
-	return res.value, res.err
+	return e.res.value, e.res.err
 }
