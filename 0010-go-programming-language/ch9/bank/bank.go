11baa95f6daa23d9298478c924339162cb6e5881
Keith Gaughan <k@stereochro.me>
2019-08-19 02:55:15 +0100

    Ch 9: switch to guard channel.

--- 0010-go-programming-language/ch9/bank/bank.go
+++ 0010-go-programming-language/ch9/bank/bank.go
@@ -1,37 +1,33 @@
 // Package bank provides a concurrency-safe bank with one account.
 package bank
 
-var deposits = make(chan int) // send amount to deposit
-var balances = make(chan int) // receive balance
+var (
+	sema    = make(chan struct{}, 1) // a binary semaphore guarding balance
+	balance int
+)
 
 func Deposit(amount int) {
-	deposits <- amount
+	sema <- struct{}{} // acquire token
+	balance += amount
+	<-sema // release token
 }
 
 func Withdraw(amount int) bool {
-	balance := Balance()
+	sema <- struct{}{} // acquire token
+	defer func() {
+		<-sema // release token
+	}()
+
 	if balance >= amount {
-		deposits <- -amount
+		balance -= amount
 		return true
 	}
 	return false
 }
 
 func Balance() int {
-	return <-balances
-}
-
-func teller() {
-	var balance int // balance is confined to teller coroutine
-	for {
-		select {
-		case amount := <-deposits:
-			balance += amount
-		case balances <- balance:
-		}
-	}
-}
-
-func init() {
-	go teller() // start the monitor goroutine
+	sema <- struct{}{} // acquire token
+	b := balance
+	<-sema // release token
+	return b
 }
