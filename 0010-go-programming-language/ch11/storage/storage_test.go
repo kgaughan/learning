6a6ee4a5789c877b33f8fbe49289d079cb2709d6
Keith Gaughan <k@stereochro.me>
2019-08-25 18:41:05 +0100

    Ch 11: storage.

---
+++ 0010-go-programming-language/ch11/storage/storage_test.go
@@ -0,0 +1,36 @@
+package storage
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCheckQuotaNotifiesUser(t *testing.T) {
+	// Save and restore original notifyUser.
+	saved := notifyUser
+	defer func() {
+		notifyUser = saved
+	}()
+
+	// Install the test's fake notifyUser.
+	var notifiedUser, notifiedMsg string
+	notifyUser = func(user, msg string) {
+		notifiedUser, notifiedMsg = user, msg
+	}
+
+	// ..simulate a 980MB-used confition...
+
+	const user = "joe@example.org"
+	CheckQuota(user)
+	if notifiedUser == "" && notifiedMsg == "" {
+		f.Fatalf("notifyUser not called")
+	}
+	if notifiedUser != user {
+		t.Errorf("wrong user (%s) notified, want %s", notifiedUser, user)
+	}
+	const wantSubstring = "98% of your quota"
+	if !strings.Contains(notifiedMsg, wantSubstring) {
+		t.Errorf("unexpected notification message <<%s>>, want substring %q",
+			notifiedMsg, wantSubstring)
+	}
+}
