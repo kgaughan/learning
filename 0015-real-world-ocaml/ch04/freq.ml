e1331b6891360fa42d64f3bd689ebeadaf97d74e
Keith Gaughan <k@stereochro.me>
2020-03-02 05:17:54 +0000

    Hide counter implementation in a module.

--- 0015-real-world-ocaml/ch04/freq.ml
+++ 0015-real-world-ocaml/ch04/freq.ml
@@ -3,16 +3,11 @@ open Stdio
 
 (** Find how often each line of `chan` occurs *)
 let build_counts chan =
-  let add_line counts line =
-    let count =
-      match List.Assoc.find ~equal:String.equal counts line with
-      | None -> 0
-      | Some x -> x
-    in List.Assoc.add ~equal:String.equal counts line (count + 1)
-  in In_channel.fold_lines chan ~init:[] ~f:add_line
+  In_channel.fold_lines chan ~init:Counter.empty ~f:Counter.touch
 
 let () =
   build_counts In_channel.stdin
+  |> Counter.to_list
   (* Sort the line frequencies in descending order *)
   |> List.sort ~compare:(fun (_, x) (_, y) -> Int.descending x y)
   (* Take the first ten lines *)
