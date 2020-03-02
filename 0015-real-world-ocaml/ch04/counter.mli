89d373a647a4483ed7d2367846cf6bfa51f070cc
Keith Gaughan <k@stereochro.me>
2020-03-02 05:40:16 +0000

    Add median.

--- 0015-real-world-ocaml/ch04/counter.mli
+++ 0015-real-world-ocaml/ch04/counter.mli
@@ -12,3 +12,12 @@ val to_list: t -> (string * int) list
 
 (** Bump the frequency count for the given string *)
 val touch: t -> string -> t
+
+(** Represents the median computed from a set of strings. In the case where
+    there is an even number of choices, the one before and after the median
+    is returned. *)
+type median =
+  | Median of string
+  | Before_and_after of string * string
+
+val median: t -> median
