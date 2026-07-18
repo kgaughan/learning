8d7001a4c971353137fd2af2683225d186819263
Keith Gaughan <k@stereochro.me>
2026-07-18 23:45:46 +0100

    Reformat

--- 0007-tour-of-git/rot13.go
+++ 0007-tour-of-git/rot13.go
@@ -23,17 +23,16 @@ func rotate(b byte) byte {
 	return lower + ((b - lower + 13) % 26)
 }
 
-func (self *rot13Reader) Read(p []byte) (n int, err error) {
-	n, err = self.r.Read(p)
+func (self *rot13Reader) Read(p []byte) (int, error) {
+	n, err := self.r.Read(p)
 	for i := 0; i < n; i++ {
 		p[i] = rotate(p[i])
 	}
-	return
+	return n, err
 }
 
 func main() {
-	s := strings.NewReader(
-		"Lbh penpxrq gur pbqr!")
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
 	io.Copy(os.Stdout, &r)
 }
